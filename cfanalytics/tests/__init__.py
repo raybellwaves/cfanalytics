b58bd9e4b03b9548f8925e81122d825c18e2d6e1
raybellwaves <rjbell1987@gmail.com>
2018-02-16 21:47:13 -0500

    updated testing

--- cfanalytics/tests/__init__.py
+++ cfanalytics/tests/__init__.py
@@ -1,8 +1,5 @@
-import requests # HTTP library
-import asyncio # Asynchronous I/O
-from aiohttp import ClientSession # Asynchronous HTTP Client/Server
+import unittest
 
 
-import pandas as pd
-import sys
-import pytest
+class TestCase(unittest.TestCase):
+    pass
