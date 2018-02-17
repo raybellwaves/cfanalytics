b61ebaf5540a1af4f988cdcacd8af4a48ed10aff
raybellwaves <rjbell1987@gmail.com>
2018-02-16 21:19:58 -0500

    Add __init__.py

---
+++ cfanalytics/tests/__init__.py
@@ -0,0 +1,8 @@
+import requests # HTTP library
+import asyncio # Asynchronous I/O
+from aiohttp import ClientSession # Asynchronous HTTP Client/Server
+
+
+import pandas as pd
+import sys
+import pytest
