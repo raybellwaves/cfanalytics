1fc9b03b04c65c95d6a88c37790581aa8e9b3d63
raybellwaves <rjbell1987@gmail.com>
2018-02-27 01:04:27 -0500

    update version number and add numpy to require

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(name='cfanalytics',
-      version='0.1.4',
+      version='0.1.5',
       license='BSD 3-Clause License',
       author='Ray Bell',      
       author_email='rbell1987@gmail.com',
       description='Downloading, analyzing and visualizing CrossFit data',
       url='https://github.com/raybellwaves/cfanalytics',
       packages=find_packages(),
-      install_requires=['requests', 'aiohttp', 'pandas'],
+      install_requires=['requests', 'aiohttp', 'pandas', 'numpy'],
       python_requires='>=3.6')
