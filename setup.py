6ec640ea6832bbe9737564cbd520320121ef10f7
raybellwaves <rjbell1987@gmail.com>
2018-03-04 12:32:28 -0500

    add xarray to requirements and up the version number

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(name='cfanalytics',
-      version='0.1.6',
+      version='0.1.7',
       license='BSD 3-Clause License',
       author='Ray Bell',      
       author_email='rbell1987@gmail.com',
       description='Downloading, analyzing and visualizing CrossFit data',
       url='https://github.com/raybellwaves/cfanalytics',
       packages=find_packages(),
-      install_requires=['requests', 'aiohttp', 'pandas', 'numpy'],
+      install_requires=['requests', 'aiohttp', 'pandas', 'numpy', 'xarray'],
       python_requires='>=3.6')
